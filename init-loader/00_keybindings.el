aa1c1dbe2135040ff0fe53119773faef8d3b3b85
WalkerSumida <walkersumida@gmail.com>
2017-01-16 19:25:25 +0900

    Added keybinds

--- init-loader/00_keybindings.el
+++ init-loader/00_keybindings.el
@@ -1,3 +1,7 @@
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
 (define-key global-map [?¥] [?\\])
 (define-key global-map (kbd "C-z") nil)
+;; g: grep
+(define-key global-map (kbd "C-x g") 'ag)
+;; re: replace
+(define-key global-map (kbd "C-x re") 'query-replace)
